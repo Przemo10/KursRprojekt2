398ca47438ae8a667ff3f80c6c6ba1e3e0ba0251
ambroziakd <ambroziak.d2@gmail.com>
2016-12-28 19:46:02 +0100

    poprawki opisów i nierówności w funkcjach

--- SuperFarmerDA/R/funkcje_pomocnicze.R
+++ SuperFarmerDA/R/funkcje_pomocnicze.R
@@ -13,7 +13,7 @@
 #' @export
 #'
 change.count <- function(game, animal, change) {
-  
+
   change = change + game[animal, "count"]
   change = max(c(0, change))
   change = min(c(change, game[animal,"max.count"]))
@@ -39,7 +39,7 @@ change.count <- function(game, animal, change) {
 exchange.farm <- function(game, farm, count){
   for (animal in farm){
     game<-change.count(game,animal, count)
-    
+
   }
   game
 }
@@ -47,7 +47,7 @@ exchange.farm <- function(game, farm, count){
 #' Wartość danego zwierzęcia liczona w królikach
 #'
 #' Funkcja \code{get.value}  zwraca wartość danego zwierzęcia liczoną w królikach.
-#' 
+#'
 #'
 #' @param game Tabela gry.
 #' @param animal Nazwa zwierzęcia.
@@ -60,14 +60,14 @@ exchange.farm <- function(game, farm, count){
 #' @export
 #'
 get.value <- function(game, animal) {
-  
+
   game[animal, "value"]
 }
 
 #' Stan obecny danego zwierzęcia
 #'
 #' Funkcja \code{get.count} zwraca liczbę zwierząt danego gatunku, posiadanych w stadzie.
-#' 
+#'
 #'
 #' @param game Tabela gry.
 #' @param animal Nazwa zwierzęcia.
@@ -80,14 +80,14 @@ get.value <- function(game, animal) {
 #' @export
 #'
 get.count <- function(game, animal) {
-  
+
   game[animal, "count"]
 }
 
 #' Zmiana stanu danego zwierzęcia na zerowy
 #'
 #' Funkcja \code{clear.count}  zeruje liczbę zwierząt danego gatunku.
-#' 
+#'
 #'
 #' @param game Tabela gry SuperFarmer.
 #' @param animal Nazwa zwierzęcia.
@@ -101,7 +101,7 @@ get.count <- function(game, animal) {
 #'
 
 clear.count <- function(game, animal) {
-  
+
  change.count(game, animal, -game[animal, "max.count"])
 
 }
@@ -109,7 +109,7 @@ clear.count <- function(game, animal) {
 #' Zmiana liczebności całego stada na zerowy
 #'
 #' Funkcja \code{clear.all.counts}  zeruje liczbę wszystkich zwierząt.
-#' 
+#'
 #'
 #' @param game Tabela gry SuperFarmer.
 #'
@@ -120,10 +120,10 @@ clear.count <- function(game, animal) {
 #'
 
 clear.all.counts <- function(game) {
-  
+
   game[,"count"] <- 0
   game
-  
+
 }
 
 
@@ -132,14 +132,14 @@ clear.all.counts <- function(game) {
 #' Funkcja \code{game.finished} czy warunek końca rozgrywki jest spełniony.
 #'
 #' @param game Tabela gry SuperFarmer.
-#' 
+#'
 #' @examples
 #' game.finished(game)
 #'
 #' @export
 #'
 game.finished <- function(game) {
-  
+
   all(game[,"count"] >= game[,"win.condition"])
 }
 
@@ -149,20 +149,20 @@ game.finished <- function(game) {
 #' Funkcja \code{exchange.two.animals} zwierzę nr 1 na zwierzę nr 2.
 #'
 #' @param game Tabela gry SuperFarmer.
-#' @param animal1 Zwierzę które chcemy zamienić.
-#' @param animal1count Liczba zwierząt które zamieniamy.
+#' @param animal1 Zwierzę które chcemy oddać.
+#' @param animal1count Liczba zwierząt które oddajemy.
 #' @param animal2 Zwierzę które chcemy dostać.
 #' @param animal2count Liczba zwierząt które powinniśmy dostać jeśli nie przekroczyliśmy limitu.
-#' 
+#'
 #' @examples
-#' exchange.two.animals(game,"pig","rabbit",1,12)
-#' exchange.two.animals(game,"pig","rabbit",1,get.value(game,"pig"))
+#' exchange.two.animals(game, "pig", "rabbit", 1, 12)
+#' exchange.two.animals(game, "pig", "rabbit", 1, get.value(game,"pig"))
 #'
 #' @export
 #'
 exchange.two.animals <- function(game, animal1, animal2, animal1count, animal2count ){
-  game<- change.count(game,animal1, -animal1count)
-  game<- change.count(game,animal2, animal2count)
+  game<- change.count(game, animal1, -animal1count)
+  game<- change.count(game, animal2, animal2count)
   game
 }
 
@@ -170,19 +170,19 @@ exchange.two.animals <- function(game, animal1, animal2, animal1count, animal2co
 #'
 #' Funkcja \code{convert.farm.vector} konwertuje wektor do tabeli game.
 #' Służy ona do umożliwienia uruchomienia innych  strategii w naszym pakiecie.
-#' 
+#'
 #' @param farm Wektor zawierający stan stada w chwili obecnej.
 #'
 #' @details Zgodnie z ustaleniami wektor, który ma być przekazywany jest innej postaci niż ustalony.
 #' Nasza tabela game ma zwierzęta w kolejności królik, mały pies, owca, świnia, duży pies, krowa, koń.
 #' W związku z tym konieczna jest funkcja przekazująca wektor do tablicy game.
-#' 
+#'
 #' @examples
 #' convert.farm.vector(c(7:1))
 #' convert.farm.vector(c(10,2,1,1,0,0,0))
 #'
 #' @export
-#' 
+#'
 #
 convert.farm.vector <- function(farm){
   game[,"count"] <- c( farm[1],farm[6],farm[2],farm[3],farm[7],farm[4],farm[5])
@@ -193,20 +193,20 @@ convert.farm.vector <- function(farm){
 #'
 #' Funkcja \code{convert.game.table} konwertuje tabelę game do wektora farm.
 #' Służy ona do umożliwienia uruchomienia naszej strategii w innych pakietach gry.
-#' 
+#'
 #' @param game Tabela game zawierająca informację dotyczącego posiadanego stada w chwili obecnej.
 #'
 #' @details Zgodnie z ustaleniami wektor, który ma być przekazywany jest innej postaci niż ustalony.
 #' Nasza tabela game ma zwierzęta w kolejności królik, mały pies, owca, świnia, duży pies, krowa, koń.
 #' W związku z tym konieczna jest funkcja konwertująca.
-#' 
+#'
 #' @examples
 #' convert.game.table(game)
 #'
 #' @export
-#' 
 #'
-convert.game.table <- function(game){ 
+#'
+convert.game.table <- function(game){
   farm <- game[c("rabbit","sheep","pig","cow","horse","small_dog","big_dog"),"count"]
   names(farm) <- c("krolik","owca","swinia","krowa","kon","maly_pies","duzy_pies")
   farm
--- SuperFarmerDA/R/strategia_duzokrolikow.R
+++ SuperFarmerDA/R/strategia_duzokrolikow.R
@@ -1,9 +1,9 @@
 #' Wartość stada w królikach
 #'
-#' Funkcja \code{value.in.rabbit} zwraca informację o wartości posiadanego statada.
+#' Funkcja \code{value.in.rabbit} zwraca informację o wartości posiadanego stada.
 #'
 #' @param game Tabela gry.
-#' 
+#'
 #' @examples
 #' value.in.rabbit(game)
 #'
@@ -11,18 +11,19 @@
 #'
 
 value.in.rabbit <- function(game){
-  
+
   sum(game[,"value"] *game[,"count"])
-  
+
 }
 
 #' Wartość stada bez wskazanych zwierząt
 #'
-#' Funkcja \code{value.in.rabbit.without.animal} zwraca informację o wartości posiadanego statada bez wskazanego zwierzęcia.
+#' Funkcja \code{value.in.rabbit.without.animal} zwraca informację
+#'  o wartości posiadanego statada bez wskazanego zwierzęcia.
 #'
 #' @param game Tabela gry.
 #' @param animal Nazwa zwierzęcia.
-#' 
+#'
 #' @examples
 #' value.in.rabbit.without.animal(game,"rabbit")
 #'
@@ -32,136 +33,154 @@ value.in.rabbit <- function(game){
 value.in.rabbit.without.animal <- function(game,animal){
   sum(game[row.names(game)!= animal,"value"] *game[row.names(game)!= animal,"count"])
 }
-  
-  
+
+
 
 #' Króliki startowe
 #'
-#' Funkcja \code{initial.rabbits} ma na celu wymianę zwierząt na króliki dopóki nie osiągniemy oczekiwanej przez nas liczby.
+#' Funkcja \code{get.initial.rabbits} ma na celu wymianę zwierząt na króliki dopóki
+#'  nie osiągniemy oczekiwanej przez nas liczby.
 #'
 #' @param game Tabela gry.
 #' @param rabbits Liczba królików, którą chcemy zachować.
-#' 
+#'
 #' @examples
 #' get.initial.rabbits(game,12)
 #'
 #' @export
 #'
- 
+
  get.initial.rabbits <- function(game, rabbits){
-   
-   if( value.in.rabbit(game) <= rabbits && get.count(game,"sheep")!= 2){
-     
+
+   if( value.in.rabbit(game) <= rabbits && get.count(game,"sheep")!= 2 &&
+       get.count(game, "small_dog")!= 2){
+
      value <- value.in.rabbit(game)
      game <- clear.all.counts(game)
      game["rabbit","count"] <- value
-     
-   }  
+
+   }
    game
  }
- 
- 
+
+
  #' Branie małych psów
  #'
- #' Funkcja \code{initial.rabbits} ma na celu wymianę zwierząt na króliki dopóki nie osiągniemy oczekiwanej przez nas liczby.
+ #' Funkcja \code{get.small.dog} wymienia króliki na małego psa.
  #'
  #' @param game Tabela gry.
- #' @param count1 Liczba królików przy których dokonujemy wymiany na psa
+ #' @param count1 Minimalna liczba królików przy których dokonujemy wymiany na małego psa
  #' @param count2 Liczba obecnie posiadanych małych psów
- #' 
+ #'
  #' @examples
  #' get.small.dog(game,41,1)
  #'
  #' @export
  #'
- 
- get.small.dog <- function(game,count1, count2){
-   
-   if(get.count(game,"rabbit") >= count1 && get.count(game,"rabbit") >6 && get.count(game,"small_dog") == count2 ){
-     
+
+ get.small.dog <- function(game, count1, count2){
+
+   if(get.count(game,"rabbit") > count1 &&
+      get.count(game,"rabbit") > 6 &&
+      get.count(game,"small_dog") == count2 ){
+
      game <-exchange.two.animals(game,"rabbit","small_dog",get.value(game,"small_dog"),1)
-     
+
    }
- game  
+ game
 }
- 
- get.more.rabbits <- function(game, maxrabbit, farm = c("sheep","pig", "big_dog")){
-   
+
+ get.more.rabbits <- function(game, maxrabbit, farm = c("sheep", "pig", "big_dog")){
+
      for (animal in farm){
-       if(get.count(game,animal)>0 && (get.value(game,animal) + get.count(game,"rabbit")) <= maxrabbit){
-         game<-  exchange.two.animals(game,animal1 = animal, animal2 = "rabbit",1,get.value(game,animal = animal))
+       if(get.count(game,animal) > 0 && (get.value(game, animal) + get.count(game, "rabbit")) <= maxrabbit){
+         game <- exchange.two.animals(game, animal1 = animal, animal2 = "rabbit", 1, get.value(game, animal = animal))
        }
-       
      }
-   
+
    game
  }
- 
- 
+
+
  #' Dodanie dodatkowego zwierzęcia
  #'
- #' Funkcja \code{get.extra.animal} zamiana króliki na zwierzęta.
- #' Zamienia ona króliki na wskazane przez nas zwierzę w sytuacji gdy mamy więcej królików od zadelkarowanej wartości.
+ #' Funkcja \code{get.extra.animal} zamienia króliki na zwierzęta w sytuacji
+ #'  gdy mamy więcej królików od zadelkarowanej wartości.
  #'
  #' @param game Tabela gry.
  #' @param rabbitcount Liczba królików przy których dokonujemy wymiany.
  #' @param animal Nazwa zwierzęcia, które chcemy dostać.
  #' @param animalcount Liczba zwierząt, które chcemy dostać.
- #' 
+ #'
  #' @examples
  #' get.extra.animal(game,45,"sheep",2)
  #'
  #' @export
  #'
- 
+
  get.extra.animal <- function(game, rabbitcount, animal, animalcount ){
-   
-  total <- animalcount * get.value(game,animal)
-    
+
+  total <- animalcount * get.value(game, animal)
+
    if(get.count(game,"rabbit") > rabbitcount && rabbitcount > total){
     game <- change.count(game,animal,animalcount)
     game <- change.count(game,"rabbit",-total)
    }
-   
+
    game
  }
- 
+
  #' Kupno konia
  #'
- #' Funkcja \code{get.extra.animal} dodaje nam konia do stada.
- #' 
+ #' Funkcja \code{buy.horse.for.animals} dodaje nam konia do stada.
+ #'
  #' @param game Tabela gry.
- #' @param rabbitcount Liczba królików, które chcemy zostawić.
- #' 
+ #' @param rabbitcount Minimalna liczba królików, które chcemy zostawić.
+ #'
  #' @examples
  #' buy.horse.for.animals(game,12)
  #'
  #' @export
  #'
- 
- buy.horse.for.animals <- function(game, rabbitcount){
-   
-   if(get.count(game,"rabbit") >= rabbitcount && value.in.rabbit(game) > (rabbitcount + get.value(game,"horse")) && get.count(game,"horse") == 0){
 
-     
+ buy.horse.for.animals <- function(game, rabbitcount){
+
+   if(value.in.rabbit(game) >= (rabbitcount + get.value(game,"horse")) &&
+      get.count(game,"horse") == 0){
+
+
      game <-  change.count(game,"rabbit",value.in.rabbit.without.animal(game,"rabbit") - get.value(game,"horse"))
      game <-  change.count(game, "horse",1)
      game <-  clear.count(game, c("small_dog","sheep","pig","cow","big_dog"))
    }
-   
-   if(value.in.rabbit.without.animal(game,"horse") >get.value(game,"horse") && get.count(game,"horse") == 1 ){
+
+   if(value.in.rabbit.without.animal(game,"horse") >= get.value(game,"horse") && get.count(game,"horse") == 1 ){
      game <- clear.all.counts(game)
      game <- change.count(game,"horse",2)
    }
    game
  }
- 
+
+ get.all <- function(game){
+   if(game["horse", "count"] == 1 && value.in.rabbit(game) >= 127){
+     value.of.others <- sum(game[c("sheep", "pig", "cow", "horse"),"value"])
+     curr.value.of.all <- value.in.rabbit(game)
+     game <- clear.all.counts(game)
+     game <- change.count(game, "rabbit", curr.value.of.all-value.of.others)
+     game <- change.count(game, "sheep", 1)
+     game <- change.count(game, "pig", 1)
+     game <- change.count(game, "cow", 1)
+     game <- change.count(game, "horse", 1)
+   }
+   game
+ }
+
  #' Strategia maxrabbit
  #'
  #' Strategia mająca na celu otrzymanie jak największej liczby królików.
- #' Funkcja \code{strategy_maxrabbit} 
+ #'
  #' @param vector Wektor z liczebnością stada.
- #' 
+ #'
  #' @return Wektor z zadaną liczebnością stada po wykonaniu strategii.
  #'
  #' @details Strategia jest strategią wiele na wiele. Składa się z następujących etapów:
@@ -177,26 +196,27 @@ value.in.rabbit.without.animal <- function(game,animal){
  #' }
  #'
  #' @author
- #' Przemysław Dycha, Dominik Ambroziak 
+ #' Przemysław Dycha, Dominik Ambroziak
  #' @examples
  #' strategy_maxrabbit(c(8,1,3,0,0,0,1))
  #'
  #' @export
  #'
- 
- 
+
+
  strategy_maxrabbit <- function(vector){
-   
+
    game <- convert.farm.vector(farm = vector)
-   
+
    game <- get.initial.rabbits(game, 12)
-   game <- get.small.dog(game, 12,0)
-   game <- get.more.rabbits(game,40)
-   game <- get.small.dog(game,41,1)
-   game <- get.extra.animal(game,40,"pig",1)
-   game <- buy.horse.for.animals(game,12)
+   game <- get.small.dog(game, 12, 0)
+   game <- get.more.rabbits(game, 40)
+   game <- get.small.dog(game, 40, 1)
+   game <- get.extra.animal(game, 40, "pig", 1)
+   game <- buy.horse.for.animals(game, 12)
+   game <- get.all(game)
    game <- exchange.horse(game)
-   
+
    vector <- convert.game.table(game)
    vector
  }
