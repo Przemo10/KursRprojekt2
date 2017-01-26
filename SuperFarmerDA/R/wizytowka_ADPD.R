7c86350e37f59bb7bacaf0b7195bb8d499ab17b0
Przemo10 <p10dycha@gmail.com>
2017-01-26 06:57:57 +0100

    korekta wizytówki

--- SuperFarmerDA/R/wizytowka_ADPD.R
+++ SuperFarmerDA/R/wizytowka_ADPD.R
@@ -18,9 +18,9 @@
 wizytowka_ADPD <- function( strategia,  N = 10000){
   
   
-  x <- SuperFarmerDA::badaj_gre( SuperFarmerDA::strategia_maxrabbit , N )[[1]]
-  y <- SuperFarmerDA::badaj_gre( strategia, N )[[1]]
-  z <- SuperFarmerDA::badaj_gre( SuperFarmerRCNK::strategia_yolo, N )[[1]]
+  wynik_strategii_max <- SuperFarmerDA::badaj_gre( SuperFarmerDA::strategia_maxrabbit , N )[[1]]
+  wynik_strategii_badanej <- SuperFarmerDA::badaj_gre( strategia, N )[[1]]
+  wynik_strategii_yolo <- SuperFarmerDA::badaj_gre( SuperFarmerRCNK::strategia_yolo, N )[[1]]
 
   strategia_max<- "maxrabbit"
   strategia_min <- "yolo"
@@ -28,16 +28,15 @@ wizytowka_ADPD <- function( strategia,  N = 10000){
   strategia_porownawcza <- substr(strategia_porownawcza,nchar("strategia_")+regexpr("strategia_",strategia_porownawcza)[[1]],nchar(strategia_porownawcza))
 
   
-  # Kolejnosc z,x,y jest istotna
-  czasy_gier <- data.frame(z,x,y)
+
+  czasy_gier <- data.frame(wynik_strategii_yolo,wynik_strategii_max,wynik_strategii_badanej)
   colnames(czasy_gier) <- c(  strategia_min,strategia_max, strategia_porownawcza)
 
 
-  ## Przeksztalcenie danych do tworzenia wykresu
+
   dane_do_wykresow <- reshape2::melt(czasy_gier)
 
-  ## WYKRES DYSTRYBUANT
-  
+### WYKRES DYSTRYBUANTY
   wykres_dystrybuanty <-
     ggplot2::ggplot(dane_do_wykresow, ggplot2::aes(x = value)) +
     ggplot2::stat_ecdf(ggplot2::aes(colour = variable)) +
@@ -54,8 +53,7 @@ wizytowka_ADPD <- function( strategia,  N = 10000){
     ggplot2::scale_x_continuous(expand = c(0, 0), limits = c(0, 260)) +
     ggplot2::theme(legend.position = "none")
   
-  ## WYKRES SKRZYPCOWY 
-  
+### WYKRES SKRZYPCOWY 
   wykres_pudelkowy <-
     ggplot2::ggplot(dane_do_wykresow, ggplot2::aes(variable, value)) +
     ggplot2::geom_violin(ggplot2::aes(fill = variable)) +
@@ -73,7 +71,7 @@ wizytowka_ADPD <- function( strategia,  N = 10000){
     ggplot2::coord_flip() +
     ggplot2::theme(legend.position = "none") 
 
-  ## Dane do statystyk
+### DANE DO TABELI
   library(dplyr)
   statystyki_strategii <-
     dane_do_wykresow %>% dplyr::group_by(variable) %>% dplyr::summarise(
@@ -89,11 +87,7 @@ wizytowka_ADPD <- function( strategia,  N = 10000){
   colnames(statystyki_strategii)[1] <- "strategia"
 
 
-  ### Procentowe przedzialy
-
-
-
-
+### PRZEDZIAŁY CZASU GRY
   dyskretne_czasy_gier <- apply(czasy_gier, 1:2, function(x)
     cut(
       x,
@@ -118,8 +112,7 @@ wizytowka_ADPD <- function( strategia,  N = 10000){
   
   
 
-    
-  
+### WYKRES PRZEDZIAŁY 
   wykres_przedzialy <- ggplot2::ggplot(d2, ggplot2::aes(Var2,Freq,fill = Var1)) +
     ggplot2::geom_bar(position = "dodge", stat = "identity") +
     ggplot2::scale_y_continuous(expand = c(0,0), labels = scales::percent ) + 
@@ -128,6 +121,7 @@ wizytowka_ADPD <- function( strategia,  N = 10000){
     ggplot2::theme_bw() + ggplot2::theme(legend.position="none") 
   
 
+### UKŁAD NA STRONIE  
   t1 <- gridExtra::ttheme_default(core=list(
     bg_params = list(fill=c(rep(c("#F8766D","#5CF058", "#619CFF"))),
                      alpha = rep(c(1,0.5), each=3))
@@ -148,10 +142,6 @@ wizytowka_ADPD <- function( strategia,  N = 10000){
   opis_slowny <- RGraphics::splitTextGrob(opis_slowny, gp = grid::gpar(fontsize = 12))
   
 
-
- 
-
-  
   gg2<-gridExtra::grid.arrange(
     autorzy,
     tytul,
@@ -188,10 +178,7 @@ wizytowka_ADPD <- function( strategia,  N = 10000){
     
   )
 
-
-  
-
-  
+### zapis pliku  
   
  nazwa_pliku_pdf <- paste0("Strategia_",strategia_porownawcza, "_wizytowka.pdf")
 
